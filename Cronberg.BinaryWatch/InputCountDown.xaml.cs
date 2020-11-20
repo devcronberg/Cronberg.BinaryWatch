557e79695f3125c17d6e3f421e1a1f7497be4ccb
Michell Cronberg <michell@cronberg.dk>
2020-11-20 16:25:09 +0100

    CountDown

---
+++ Cronberg.BinaryWatch/InputCountDown.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cronberg.BinaryWatch
+{
+    /// <summary>
+    /// Interaction logic for InputCountDown.xaml
+    /// </summary>
+    public partial class InputCountDown : Window
+    {
+        public int CountToMinutes { get; set; }
+        public TimeSpan CountToTime { get; set; }
+        public InputCountDown()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if(txtMinutes.Text !="")
+                    CountToMinutes = Convert.ToInt32(txtMinutes.Text);
+                if(txtCountTo.Text!="")
+                    CountToTime = new TimeSpan(Convert.ToInt32(txtCountTo.Text.Split(":")[0]), Convert.ToInt32(txtCountTo.Text.Split(":")[1]),0);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            this.Close();
+        }
+    }
+}
--- Cronberg.BinaryWatch/MainWindow.xaml.cs
+++ Cronberg.BinaryWatch/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ namespace Cronberg.BinaryWatch
     {
 
         Options option = new Options();
-
+        DateTime countDownTo;
         public MainWindow()
         {
             InitializeComponent();
@@ -22,34 +22,74 @@ namespace Cronberg.BinaryWatch
             this.WindowStartupLocation = WindowStartupLocation.CenterOwner;
             this.WindowState = WindowState.Normal;
             this.Title = "Cronbergs BinaryWatch";
-            
-            ShowValue();
+
+            ShowTimeValue();
 
             System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += (s, e) =>
             {
-                ShowValue();
+                ShowTimeValue();
             };
             dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
             dispatcherTimer.Start();
+
+            myMenu.Visibility = Visibility.Visible;
         }
 
-        public void ShowValue()
+        public void ShowTimeValue()
         {
             string h = "";
             string m = "";
             string s = "";
             if (!option.ShowHex)
             {
-                h = Convert.ToString(DateTime.Now.Hour, 2).ToString().PadLeft(8, '0');
-                m = Convert.ToString(DateTime.Now.Minute, 2).ToString().PadLeft(8, '0');
-                s = Convert.ToString(DateTime.Now.Second, 2).ToString().PadLeft(8, '0');
+                if (option.CountDown)
+                {
+                    if (countDownTo > DateTime.Now)
+                    {
+                        h = Convert.ToString((countDownTo - DateTime.Now).Hours, 2).PadLeft(8, '0');
+                        m = Convert.ToString((countDownTo - DateTime.Now).Minutes, 2).PadLeft(8, '0');
+                        s = Convert.ToString((countDownTo - DateTime.Now).Seconds, 2).PadLeft(8, '0');
+                    }
+                    else
+                    {
+                        h = "00000000";
+                        m = "00000000";
+                        s = "00000000";
+                    }
+                }
+                else
+                {
+                    h = Convert.ToString(DateTime.Now.Hour, 2).ToString().PadLeft(8, '0');
+                    m = Convert.ToString(DateTime.Now.Minute, 2).ToString().PadLeft(8, '0');
+                    s = Convert.ToString(DateTime.Now.Second, 2).ToString().PadLeft(8, '0');
+                }
             }
             else
             {
-                h = Convert.ToString(DateTime.Now.Hour, 16).ToString().PadLeft(2, '0');
-                m = Convert.ToString(DateTime.Now.Minute, 16).ToString().PadLeft(2, '0');
-                s = Convert.ToString(DateTime.Now.Second, 16).ToString().PadLeft(2, '0');
+                if (option.CountDown)
+                {
+                    if (countDownTo > DateTime.Now)
+                    {
+
+                        h = Convert.ToString((countDownTo - DateTime.Now).Hours, 16).PadLeft(2, '0');
+                        m = Convert.ToString((countDownTo - DateTime.Now).Minutes, 16).PadLeft(2, '0');
+                        s = Convert.ToString((countDownTo - DateTime.Now).Seconds, 16).PadLeft(2, '0');
+                    }
+                    else
+                    {
+                        h = "00";
+                        m = "00";
+                        s = "00";
+                    }
+
+                }
+                else
+                {
+                    h = Convert.ToString(DateTime.Now.Hour, 16).ToString().PadLeft(2, '0');
+                    m = Convert.ToString(DateTime.Now.Minute, 16).ToString().PadLeft(2, '0');
+                    s = Convert.ToString(DateTime.Now.Second, 16).ToString().PadLeft(2, '0');
+                }
             }
             if (option.ShowSeconds)
                 this.txt.Text = $"{h}:{m}:{s}".ToUpper();
@@ -58,7 +98,7 @@ namespace Cronberg.BinaryWatch
 
             if (!option.ShowHex)
             {
-                
+
                 this.Title = "Cronbergs BinaryWatch";
                 if (option.ShowSeconds)
                 {
@@ -81,7 +121,16 @@ namespace Cronberg.BinaryWatch
                     this.Width = 200;
                 }
             }
-            this.Topmost = option.TopMost;            
+            this.Topmost = option.TopMost;
+            if (!option.ShowTitle)
+            {
+                this.WindowStyle = WindowStyle.SingleBorderWindow;
+            }
+            else
+            {
+                this.WindowStyle = WindowStyle.None;
+            }
+
         }
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
@@ -92,28 +141,58 @@ namespace Cronberg.BinaryWatch
         }
 
 
+        private void menuCountDown(object sender, RoutedEventArgs e)
+        {
+            option.CountWatch = false;
+            option.CountDown = true;
+            var s = this.Topmost;
+            InputCountDown i = new InputCountDown();
+            option.TopMost = false;
+            i.Topmost = true;
+            i.ShowDialog();
+            option.TopMost = s;
+            if (i.CountToMinutes > 0)
+                countDownTo = DateTime.Now.AddMinutes(i.CountToMinutes + 1);
+            if (i.CountToTime != new TimeSpan(0, 0, 0))
+                countDownTo = DateTime.Now.Date + i.CountToTime;
+
+            myMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void menuShowTitle(object sender, RoutedEventArgs e)
+        {
+            option.ShowTitle = !option.ShowTitle;
+            myMenu.Visibility = Visibility.Collapsed;
+        }
+
         private void menuExit(object sender, RoutedEventArgs e)
         {
+            this.Close();
+        }
+        private void menuWatch(object sender, RoutedEventArgs e)
+        {
+            option.CountWatch = true;
+            option.CountDown = false;
             this.Close();
         }
         private void menuShowSeconds(object sender, RoutedEventArgs e)
         {
             option.ShowSeconds = !option.ShowSeconds;
             myMenu.Visibility = Visibility.Collapsed;
-            ShowValue();
+            ShowTimeValue();
         }
         private void menuShowHex(object sender, RoutedEventArgs e)
         {
             option.ShowHex = !option.ShowHex;
             myMenu.Visibility = Visibility.Collapsed;
-            ShowValue();
+            ShowTimeValue();
         }
 
         private void menuTopMost(object sender, RoutedEventArgs e)
         {
             option.TopMost = !option.TopMost;
             myMenu.Visibility = Visibility.Collapsed;
-            ShowValue();
+            ShowTimeValue();
         }
 
     }
@@ -123,5 +202,8 @@ namespace Cronberg.BinaryWatch
         public bool ShowSeconds { get; set; }
         public bool ShowHex { get; set; }
         public bool TopMost { get; set; } = true;
+        public bool CountDown { get; set; }
+        public bool CountWatch { get; set; } = true;
+        public bool ShowTitle { get; set; }
     }
 }
